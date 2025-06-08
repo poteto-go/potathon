414e44bd8b9b211258a4ef45d0e06dc2db4d2ba5
eaggle23 <n.taka523@gmail.com>
2025-06-08 16:44:59 +0900

    【calculator】バイナリを実行

---
+++ calculator/app/_sample/sample_code.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a, b; cin >> a >> b;
+  cerr << "Error output:" << a << " " << b << endl; // This will output to standard error
+  cout << a + b << endl; // This will output to standard output
+  return 0;
+}
